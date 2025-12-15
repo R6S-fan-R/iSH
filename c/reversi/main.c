7bbd70c486a9f8ebaf2c3630b87d36332c8769ae
Rshi <rshichannel@gmail.com>
2025-12-15 18:38:50 +0900

    入力部分

--- c/reversi/main.c
+++ c/reversi/main.c
@@ -3,7 +3,7 @@
  int main (void)
 {
     int B[8][8];
-    int i, j, k;
+    int i, j;
     for(i = 0; i < 8; i ++)
     {
         for(j = 0; j < 8; j ++)
@@ -15,9 +15,32 @@
     }
 
     int scanx, scany;
+    char yesno = '0';
     scanf("%d", &scanx);
     scanf("%d", &scany);
-    B[scanx][scany] = 2;
+    printf("x = %d, y = %d \n y/n", scanx, scany);
+    scanf(" %c", &yesno);
+    if(yesno == 'y')
+    {
+        B[scanx][scany] = 2;
+
+        for(i = 0; i < 8; i ++)
+        {
+            for(j = 0; j < 8; j ++)
+                {
+                    B[i][j] = 0;
+                    printf("%d ", B[i][j]);
+                }
+        printf("\n");
+        }
+
+    }
+    else{
+        scanf("%d", &scanx);
+        scanf("%d", &scany);
+        printf("x = %d, y = %d \n y/n", scanx, scany);
+        scanf("%c", &yesno);
+        }
 
     return 0;
 }
